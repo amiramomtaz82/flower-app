36294fdc64637070d2d314341eb8212762bd4873
HadiHeikal <hadiiheikal@gmail.com>
2026-08-13 10:05:06 +0300

    feat: Add views for login, register, reset password, and forget password

---
+++ lib/features/auth/presentation/forget_password/views/otp_verification_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OtpVerificationView extends StatelessWidget {
+  const OtpVerificationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
