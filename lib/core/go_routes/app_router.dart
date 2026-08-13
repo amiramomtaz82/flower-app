e448a02a6253746b5fe186718179f2777df2cdd0
Amira Momtaz <amiraomtaz82@gmail.com>
2026-08-13 11:06:45 +0300

    AooRouter

--- lib/core/go_routes/app_router.dart
+++ lib/core/go_routes/app_router.dart
@@ -1,10 +1,31 @@
+import 'package:flower_app/core/go_routes/routes_name.dart';
+import 'package:flower_app/features/auth/presentation/forget_password/views/forget_password_view.dart';
 import 'package:go_router/go_router.dart';
+
+import '../../features/auth/presentation/login/views/login_view.dart';
+import '../../features/home/presentation/home_view.dart';
+
 
 class AppRouter {
   AppRouter._();
 
   static final GoRouter router = GoRouter(
-    // initialLocation:,
-    routes: [],
+    initialLocation: AppRoutes.login,
+
+    routes: [
+      GoRoute(
+        path: AppRoutes.login,
+        builder: (context, state) => const LoginView(),
+      ),
+      GoRoute(
+        path: AppRoutes.forgotPassword,
+        builder: (context, state) => const ForgetPasswordView(),
+      ),
+
+      GoRoute(
+        path: AppRoutes.home,
+        builder: (context, state) => const HomeView(),
+      ),
+    ],
   );
 }
