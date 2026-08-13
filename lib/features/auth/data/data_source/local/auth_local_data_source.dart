61ee0a6858176301f70dea43dc7493fa5b618e58
HadiHeikal <hadiiheikal@gmail.com>
2026-08-13 08:13:41 +0300

    feat: Add AuthLocalDataSource and AuthRemoteDataSource interfaces for authentication flow

---
+++ lib/features/auth/data/data_source/local/auth_local_data_source.dart
@@ -0,0 +1,5 @@
+abstract interface class AuthLocalDataSource {
+  Future<void> saveToken(String token);
+  Future<String?> getToken();
+  Future<void> clearToken();
+}
---
+++ lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
@@ -0,0 +1,9 @@
+abstract interface class AuthRemoteDataSource {
+  // TODO: implement login
+
+  // TODO: implement register
+
+  // TODO: implement forgetPassword
+
+  // TODO: implement resetPassword
+}
