d0c7c2d79c808b9c4e1d8550ef2db06e786f7e9f
Amira Momtaz <amiraomtaz82@gmail.com>
2026-08-13 20:00:43 +0300

    update theme, and local remote data source

--- lib/features/auth/api/data_source_impl/local/auth_local_data_source_impl.dart
+++ lib/features/auth/api/data_source_impl/local/auth_local_data_source_impl.dart
@@ -1,19 +1,43 @@
 import 'package:flower_app/config/secure_storage/secure_storage.dart';
 import 'package:flower_app/features/auth/data/data_source/local/auth_local_data_source.dart';
+
 import 'package:injectable/injectable.dart';
+
+import '../../../data/models/login_response.dart';
 
 @LazySingleton(as: AuthLocalDataSource)
 class AuthLocalDataSourceImpl implements AuthLocalDataSource {
   final SecureStorage _secureStorage;
 
+
   AuthLocalDataSourceImpl(this._secureStorage);
 
   @override
-  Future<void> saveToken(String token) => _secureStorage.saveToken(token);
+  Future<void> saveToken(String token) => _secureStorage.saveAccessToken(token);
 
   @override
-  Future<String?> getToken() => _secureStorage.getToken();
+  Future<String?> getToken() => _secureStorage.getAccessToken();
 
   @override
-  Future<void> clearToken() => _secureStorage.deleteToken();
+  Future<void> clearToken() => _secureStorage.deleteAccessToken();
+
+  @override
+  Future<void> clearAuthData() =>_secureStorage.clearAuthData();
+
+
+  @override
+  Future<String?> getRefreshToken() => _secureStorage.getRefreshToken();
+
+
+
+  @override
+  Future<User?> getUser() =>_secureStorage.getUser();
+
+
+
+  @override
+  Future<void> saveRefreshToken(String token) =>_secureStorage.saveRefreshToken(token);
+
+  @override
+  Future<void> saveUser(User user) =>_secureStorage.saveUser(user);
 }
--- lib/features/auth/data/data_source/local/auth_local_data_source.dart
+++ lib/features/auth/data/data_source/local/auth_local_data_source.dart
@@ -1,5 +1,32 @@
+
+import '../../models/login_response.dart';
+
 abstract interface class AuthLocalDataSource {
+
+
   Future<void> saveToken(String token);
+
+  Future<void> saveRefreshToken(String token);
+
+  Future<void> saveUser(User user);
+
   Future<String?> getToken();
+
+
+
+  Future<String?> getRefreshToken();
+
+
+
+  Future<User?> getUser();
+
+
   Future<void> clearToken();
+
+  Future<void> clearAuthData();
+
+
+
 }
+
+
