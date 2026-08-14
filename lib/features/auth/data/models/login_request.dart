575c34e424e4e32fdb15f7d98b588204b913eaed
Amira Momtaz <amiraomtaz82@gmail.com>
2026-08-14 17:52:09 +0300

    adding obscuretext to password field

--- lib/features/auth/data/models/login_request.dart
+++ lib/features/auth/data/models/login_request.dart
@@ -1,22 +1,32 @@
 /// email : "customer@example.com"
 /// password : "Password123"
+/// deviceId : "device_abc123"
+/// fcmToken : "fcm_token_xyz"
 
 class LoginRequest {
   LoginRequest({
-      this.email, 
-      this.password,});
+    this.email,
+    this.password,
+    this.deviceId,
+    this.fcmToken,});
 
   LoginRequest.fromJson(dynamic json) {
     email = json['email'];
     password = json['password'];
+    deviceId = json['deviceId'];
+    fcmToken = json['fcmToken'];
   }
   String? email;
   String? password;
+  String? deviceId;
+  String? fcmToken;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['email'] = email;
     map['password'] = password;
+    map['deviceId'] = deviceId;
+    map['fcmToken'] = fcmToken;
     return map;
   }
 
