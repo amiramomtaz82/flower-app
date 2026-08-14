67bc53fdf6708f796fe3f21e1f4836b4c876112a
HadiHeikal <hadiiheikal@gmail.com>
2026-08-14 21:02:24 +0300

    add endpoints for password recovery and verification

--- lib/core/app_constants/endpoints.dart
+++ lib/core/app_constants/endpoints.dart
@@ -2,4 +2,8 @@ class Endpoints {
   Endpoints._();
 
   static const String baseUrl = '';
+
+  static const String forgetPassword = '/auth/forgot-password';
+  static const String verifyOtp = '/auth/verify-otp';
+  static const String resetPassword = '/auth/reset-password';
 }
