c14105e63d860ef9c96949631c0590cb1643f06e
HadiHeikal <hadiiheikal@gmail.com>
2026-08-15 11:25:20 +0300

    implement logic for forgetPassword and resetPassword use cases

--- lib/features/auth/domain/use_cases/reset_password_use_case.dart
+++ lib/features/auth/domain/use_cases/reset_password_use_case.dart
@@ -1,3 +1,5 @@
+import 'package:flower_app/config/base_response/base_response.dart';
+import 'package:flower_app/features/auth/domain/entities/auth_message_entity.dart';
 import 'package:flower_app/features/auth/domain/repo/auth_repo.dart';
 import 'package:injectable/injectable.dart';
 
@@ -5,5 +7,16 @@ import 'package:injectable/injectable.dart';
 class ResetPasswordUseCase {
   final AuthRepo _authRepo;
   ResetPasswordUseCase(this._authRepo);
-  // TODO: implement reset password use case logic
+
+  Future<BaseResponse<AuthMessageEntity>> call({
+    required String resetToken,
+    required String newPassword,
+    required String confirmNewPassword,
+  }) {
+    return _authRepo.resetPassword(
+      resetToken: resetToken,
+      newPassword: newPassword,
+      confirmNewPassword: confirmNewPassword,
+    );
+  }
 }
