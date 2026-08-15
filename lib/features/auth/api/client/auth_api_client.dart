1ca04ea351caa5b280ba09559e0cc76da93d42cd
HadiHeikal <hadiiheikal@gmail.com>
2026-08-15 10:49:00 +0300

    implement forgetPassword, verifyOtp, and resetPassword methods in AuthApiClient

--- lib/features/auth/api/client/auth_api_client.dart
+++ lib/features/auth/api/client/auth_api_client.dart
@@ -1,4 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:flower_app/core/app_constants/endpoints.dart';
+import 'package:flower_app/features/auth/data/models/forgot_password_request_model.dart';
+import 'package:flower_app/features/auth/data/models/message_response_model.dart';
+import 'package:flower_app/features/auth/data/models/reset_password_request_model.dart';
+import 'package:flower_app/features/auth/data/models/verify_otp_request_model.dart';
+import 'package:flower_app/features/auth/data/models/verify_otp_response_model.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 part 'auth_api_client.g.dart';
@@ -13,7 +19,16 @@ abstract class AuthApiClient {
 
   // TODO: implement register
 
-  // TODO: implement forgetPassword
+  @POST(Endpoints.forgetPassword)
+  Future<MessageResponseModel> forgetPassword(
+    @Body() ForgotPasswordRequestModel body,
+  );
 
-  // TODO: implement resetPassword
+  @POST(Endpoints.verifyOtp)
+  Future<VerifyOtpResponseModel> verifyOtp(@Body() VerifyOtpRequestModel body);
+
+  @POST(Endpoints.resetPassword)
+  Future<MessageResponseModel> resetPassword(
+    @Body() ResetPasswordRequestModel body,
+  );
 }
