1985ad064dad6e27622b58a32b44ba2ed5f9a0fc
HadiHeikal <hadiiheikal@gmail.com>
2026-08-15 11:06:02 +0300

    implement forgetPassword, verifyOtp, and resetPassword methods in AuthRemoteDataSourceImpl

--- lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
+++ lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
@@ -1,9 +1,21 @@
+import 'package:flower_app/features/auth/data/models/message_response_model.dart';
+import 'package:flower_app/features/auth/data/models/verify_otp_response_model.dart';
+
 abstract interface class AuthRemoteDataSource {
   // TODO: implement login
 
   // TODO: implement register
 
-  // TODO: implement forgetPassword
+  Future<MessageResponseModel> forgetPassword({required String email});
 
-  // TODO: implement resetPassword
+  Future<VerifyOtpResponseVm> verifyOtp({
+    required String email,
+    required String otpCode,
+  });
+
+  Future<MessageResponseModel> resetPassword({
+    required String resetToken,
+    required String newPassword,
+    required String confirmNewPassword,
+  });
 }
