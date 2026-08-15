92c8c8d1c50ddb07f98030d7401922c0c91ffa1d
HadiHeikal <hadiiheikal@gmail.com>
2026-08-15 11:23:01 +0300

    implement forgetPassword, verifyOtp, and resetPassword methods in AuthRepoImpl

--- lib/features/auth/data/repo_impl/auth_repo_impl.dart
+++ lib/features/auth/data/repo_impl/auth_repo_impl.dart
@@ -1,5 +1,8 @@
+import 'package:flower_app/config/base_response/base_response.dart';
 import 'package:flower_app/features/auth/data/data_source/local/auth_local_data_source.dart';
 import 'package:flower_app/features/auth/data/data_source/remote/auth_remote_data_source.dart';
+import 'package:flower_app/features/auth/domain/entities/auth_message_entity.dart';
+import 'package:flower_app/features/auth/domain/entities/reset_token_entity.dart';
 import 'package:flower_app/features/auth/domain/repo/auth_repo.dart';
 import 'package:injectable/injectable.dart';
 
@@ -13,7 +16,51 @@ class AuthRepoImpl implements AuthRepo {
 
   // TODO: implement login
 
-  // TODO: implement forgetPassword
+  @override
+  Future<BaseResponse<AuthMessageEntity>> forgetPassword({
+    required String email,
+  }) async {
+    try {
+      final response = await _authRemoteDataSource.forgetPassword(email: email);
+      return SuccessResponse(response.toEntity());
+    } on Exception catch (e) {
+      return ErrorResponse(error: e);
+    }
+  }
 
-  // TODO: implement resetPassword
+  @override
+  Future<BaseResponse<ResetToken>> verifyOtp({
+    required String email,
+    required String otpCode,
+  }) async {
+    try {
+      final response = await _authRemoteDataSource.verifyOtp(
+        email: email,
+        otpCode: otpCode,
+      );
+      return SuccessResponse(response.toEntity());
+    } on Exception catch (e) {
+      return ErrorResponse(error: e);
+    }
+  }
+
+  @override
+  Future<BaseResponse<AuthMessageEntity>> resetPassword({
+    required String resetToken,
+    required String newPassword,
+    required String confirmNewPassword,
+  }) async {
+    try {
+      final response = await _authRemoteDataSource.resetPassword(
+        resetToken: resetToken,
+        newPassword: newPassword,
+        confirmNewPassword: confirmNewPassword,
+      );
+      // clear auth data when reset password success to avoid user relogin
+      await _authLocalDataSource.clearAuthData();
+      return SuccessResponse(response.toEntity());
+    } on Exception catch (e) {
+      return ErrorResponse(error: e);
+    }
+  }
 }
--- lib/features/auth/domain/repo/auth_repo.dart
+++ lib/features/auth/domain/repo/auth_repo.dart
@@ -1,9 +1,24 @@
+import 'package:flower_app/config/base_response/base_response.dart';
+import 'package:flower_app/features/auth/domain/entities/auth_message_entity.dart';
+import 'package:flower_app/features/auth/domain/entities/reset_token_entity.dart';
+
 abstract interface class AuthRepo {
   // TODO: implement register
 
   // TODO: implement login
 
-  // TODO: implement forgetPassword
+  Future<BaseResponse<AuthMessageEntity>> forgetPassword({
+    required String email,
+  });
 
-  // TODO: implement resetPassword
+  Future<BaseResponse<ResetToken>> verifyOtp({
+    required String email,
+    required String otpCode,
+  });
+
+  Future<BaseResponse<AuthMessageEntity>> resetPassword({
+    required String resetToken,
+    required String newPassword,
+    required String confirmNewPassword,
+  });
 }
