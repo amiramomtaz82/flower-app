9ed5b721b9cc6e744137c55ed9ce5c783c49539d
amgad ahmed <13239659amgad@gmail.com>
2026-08-17 22:02:59 +0300

    remove login route from AppRouter

--- lib/core/go_routes/app_router.dart
+++ lib/core/go_routes/app_router.dart
@@ -3,7 +3,7 @@ import 'package:flower_app/features/auth/presentation/forget_password/views/forg
 import 'package:flower_app/features/auth/presentation/register/views/register_view.dart';
 import 'package:go_router/go_router.dart';
 
-import '../../features/auth/presentation/login/views/login_view.dart';
+
 import '../../features/home/presentation/home_view.dart';
 
 class AppRouter {
@@ -13,10 +13,7 @@ class AppRouter {
     initialLocation: AppRoutes.register,
 
     routes: [
-      GoRoute(
-        path: AppRoutes.login,
-        builder: (context, state) => const LoginView(),
-      ),
+
       GoRoute(
         path: AppRoutes.register,
         builder: (context, state) => const RegisterView(),
