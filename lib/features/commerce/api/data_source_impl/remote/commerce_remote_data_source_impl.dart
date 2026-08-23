0423b856d8c76c88f27238f1967d3c8126727bef
HadiHeikal <hadiiheikal@gmail.com>
2026-08-23 18:31:41 +0300

    feat: implement methods for fetching categories, occasions, products, and product by ID in CommerceRemoteDataSource

--- lib/features/commerce/api/data_source_impl/remote/commerce_remote_data_source_impl.dart
+++ lib/features/commerce/api/data_source_impl/remote/commerce_remote_data_source_impl.dart
@@ -1,9 +1,63 @@
-
-
-
 import 'package:injectable/injectable.dart';
 
 import '../../../data/data_sorce/remote/commerce_remote_data_source.dart';
+import '../../../data/models/category_dto.dart';
+import '../../../data/models/occasions_data_model.dart';
+import '../../../data/models/product_dto.dart';
+import '../../../data/models/products_data_model.dart';
+import '../../client/commerce_api_client.dart';
 
-@Injectable(as:CommerceRemoteDataSource)
-class CommerceRemoteDataSourceImpl implements CommerceRemoteDataSource{}
+@Injectable(as: CommerceRemoteDataSource)
+class CommerceRemoteDataSourceImpl implements CommerceRemoteDataSource {
+  final CommerceApiClient _commerceApiClient;
+
+  CommerceRemoteDataSourceImpl(this._commerceApiClient);
+
+  @override
+  Future<List<CategoryDTO>> getCategories() async {
+    final response = await _commerceApiClient.getCategories();
+    return response.data;
+  }
+
+  @override
+  Future<OccasionsDataModel> getOccasions({
+    required int pageNumber,
+    required int pageSize,
+  }) async {
+    final response = await _commerceApiClient.getOccasions(
+      pageNumber,
+      pageSize,
+    );
+    return response.data;
+  }
+
+  @override
+  Future<ProductsDataModel> getProducts({
+    String? occasionId,
+    required int pageNumber,
+    required int pageSize,
+  }) async {
+    final response = await _commerceApiClient.getProducts(
+      occasionId,
+      pageNumber,
+      pageSize,
+    );
+    return response.data;
+  }
+
+  @override
+  Future<List<ProductDTO>> getProductsByCategory({
+    required String categoryId,
+  }) async {
+    final response = await _commerceApiClient.getProductsByCategory(
+      categoryId,
+    );
+    return response.data;
+  }
+
+  @override
+  Future<ProductDTO> getProductById({required String productId}) async {
+    final response = await _commerceApiClient.getProductById(productId);
+    return response.data;
+  }
+}
--- lib/features/commerce/data/data_sorce/remote/commerce_remote_data_source.dart
+++ lib/features/commerce/data/data_sorce/remote/commerce_remote_data_source.dart
@@ -1,3 +1,23 @@
+import '../../models/category_dto.dart';
+import '../../models/occasions_data_model.dart';
+import '../../models/product_dto.dart';
+import '../../models/products_data_model.dart';
 
+abstract interface class CommerceRemoteDataSource {
+  Future<List<CategoryDTO>> getCategories();
 
-abstract interface class CommerceRemoteDataSource {}
+  Future<OccasionsDataModel> getOccasions({
+    required int pageNumber,
+    required int pageSize,
+  });
+
+  Future<ProductsDataModel> getProducts({
+    String? occasionId,
+    required int pageNumber,
+    required int pageSize,
+  });
+
+  Future<List<ProductDTO>> getProductsByCategory({required String categoryId});
+
+  Future<ProductDTO> getProductById({required String productId});
+}
