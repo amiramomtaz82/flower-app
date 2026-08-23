2cc6f2bdf204fbfaf91faf4a84cccb9853cf294f
HadiHeikal <hadiiheikal@gmail.com>
2026-08-23 16:21:38 +0300

    feat: add dummy data for categories, products, and occasions

--- lib/features/commerce/api/data_source_impl/local/dummy_data.dart
+++ lib/features/commerce/api/data_source_impl/local/dummy_data.dart
@@ -1,110 +1,28 @@
 import '../../../domain/entities/product_entity.dart';
 
-List<ProductEntity> dummyList=[ProductEntity(
-    id: "1",
-    price:200,
-    name: "Red roses",
-    currency: "Egp",
-    discountPercentage: 20,
-    originalPrice: 240,
-    imageUrl: "https://images.unsplash.com/photo-1490750967868-88aa4486c946",
-    status: "inStock"
-
-),
+List<ProductEntity> dummyList = const [
   ProductEntity(
-      id: "1",
-      price:200,
-      name:  "Red roses",
-      currency: "Egp",
-      discountPercentage: 20,
-      originalPrice: 240,
-      imageUrl: "assets/images/ic_notification.png",
-      status: "inStock"
-
+    id: '1',
+    name: 'Sunny',
+    price: 600,
+    currency: 'EGP',
+    imageUrl: 'https://images.unsplash.com/photo-1490750967868-88aa4486c946',
+    status: 'inStock',
   ),
   ProductEntity(
-      id: "1",
-      price:200,
-      name: "red rose",
-      currency: "Egp",
-      discountPercentage: 20,
-      originalPrice: 240,
-      imageUrl: "assets/images/ic_notification.png",
-      status: "inStock"
-
-  ),ProductEntity(
-      id: "1",
-      price:200,
-      name: "red rose",
-      currency: "Egp",
-      discountPercentage: 20,
-      originalPrice: 240,
-      imageUrl: "assets/images/ic_notification.png",
-      status: "inStock"
-
+    id: '2',
+    name: 'Red roses',
+    price: 600,
+    currency: 'EGP',
+    imageUrl: 'https://images.unsplash.com/photo-1518895949257-7621c3c786d7',
+    status: 'inStock',
   ),
   ProductEntity(
-      id: "1",
-      price:200,
-      name: "red rose",
-      currency: "Egp",
-      discountPercentage: 20,
-      originalPrice: 240,
-      imageUrl: "assets/images/ic_notification.png",
-      status: "inStock"
-
-  ),ProductEntity(
-      id: "1",
-      price:200,
-      name: "red rose",
-      currency: "Egp",
-      discountPercentage: 20,
-      originalPrice: 240,
-      imageUrl: "assets/images/ic_notification.png",
-      status: "inStock"
-
-  ),ProductEntity(
-      id: "1",
-      price:200,
-      name: "red rose",
-      currency: "Egp",
-      discountPercentage: 20,
-      originalPrice: 240,
-      imageUrl: "assets/images/ic_notification.png",
-      status: "inStock"
-
-  ),ProductEntity(
-      id: "1",
-      price:200,
-      name: "red rose",
-      currency: "Egp",
-      discountPercentage: 20,
-      originalPrice: 240,
-      imageUrl: "assets/images/ic_notification.png",
-      status: "inStock"
-
-  ),ProductEntity(
-      id: "1",
-      price:200,
-      name: "red rose",
-      currency: "Egp",
-      discountPercentage: 20,
-      originalPrice: 240,
-      imageUrl: "assets/images/ic_notification.png",
-      status: "inStock"
-
-  ),ProductEntity(
-      id: "1",
-      price:200,
-      name: "red rose",
-      currency: "Egp",
-      discountPercentage: 20,
-      originalPrice: 240,
-      imageUrl: "assets/images/ic_notification.png",
-      status: "inStock"
-
-  )
-
-
-
+    id: '3',
+    name: 'Spring vase',
+    price: 600,
+    currency: 'EGP',
+    imageUrl: 'https://images.unsplash.com/photo-1526047932273-341f2a7631f9',
+    status: 'inStock',
+  ),
 ];
