448190f7ec93159eae7fe37d63ea0bee5a08ae0c
HadiHeikal <hadiiheikal@gmail.com>
2026-08-23 18:29:31 +0300

    feat: update CommerceApiClient to include endpoints for categories, occasions, and products

--- lib/features/commerce/api/client/commerce_api_client.dart
+++ lib/features/commerce/api/client/commerce_api_client.dart
@@ -1,8 +1,13 @@
 import 'package:dio/dio.dart';
+import 'package:flower_app/core/app_constants/endpoints.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 
-import '../../data/models/ProductResponse.dart';
+import '../../data/models/categories_response_model.dart';
+import '../../data/models/occasions_response_model.dart';
+import '../../data/models/product_response_model.dart';
+import '../../data/models/products_by_category_response_model.dart';
+import '../../data/models/products_response_model.dart';
 part 'commerce_api_client.g.dart';
 
 @singleton
@@ -11,12 +16,29 @@ abstract class CommerceApiClient {
   @factoryMethod
   factory CommerceApiClient(Dio dio) = _CommerceApiClient;
 
-  @GET("/products")
-  Future<ProductResponse> getProducts(
-      @Query('page') int? page,
-      @Query('pageSize') int? pageSize,
-      );
+  @GET(Endpoints.categories)
+  Future<CategoriesResponseModel> getCategories();
 
+  @GET(Endpoints.occasions)
+  Future<OccasionsResponseModel> getOccasions(
+    @Query('pageNumber') int pageNumber,
+    @Query('pageSize') int pageSize,
+  );
 
+  @GET(Endpoints.products)
+  Future<ProductsResponseModel> getProducts(
+    @Query('occasionId') String? occasionId,
+    @Query('pageNumber') int pageNumber,
+    @Query('pageSize') int pageSize,
+  );
 
+  @GET(Endpoints.productsByCategory)
+  Future<ProductsByCategoryResponseModel> getProductsByCategory(
+    @Query('categoryId') String categoryId,
+  );
+
+  @GET(Endpoints.productById)
+  Future<ProductResponseModel> getProductById(
+    @Path('productId') String productId,
+  );
 }
