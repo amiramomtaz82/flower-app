c0876e5ee02d2c125e0416ed613e7e73a9c5fadd
HadiHeikal <hadiiheikal@gmail.com>
2026-08-23 16:15:51 +0300

    feat: implement CommerceRepo interface with methods for categories, occasions, and products

--- lib/features/commerce/domain/repo/commerce_repo.dart
+++ lib/features/commerce/domain/repo/commerce_repo.dart
@@ -1,4 +1,29 @@
+import 'package:flower_app/config/base_response/base_response.dart';
+import 'package:flower_app/core/pagination/paginated_response.dart';
 
+import '../entities/category_entity.dart';
+import '../entities/occasion_entity.dart';
+import '../entities/product_entity.dart';
 
+abstract interface class CommerceRepo {
+  Future<BaseResponse<List<CategoryEntity>>> getCategories();
 
-abstract interface class CommerceRepo {}
+  Future<BaseResponse<PaginatedResponse<OccasionEntity>>> getOccasions({
+    required int pageNumber,
+    required int pageSize,
+  });
+
+  Future<BaseResponse<PaginatedResponse<ProductEntity>>> getProducts({
+    String? occasionId,
+    required int pageNumber,
+    required int pageSize,
+  });
+
+  Future<BaseResponse<List<ProductEntity>>> getProductsByCategory({
+    required String categoryId,
+  });
+
+  Future<BaseResponse<ProductEntity>> getProductById({
+    required String productId,
+  });
+}
