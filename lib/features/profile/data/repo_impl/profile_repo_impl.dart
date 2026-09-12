ce530125445e01381c6fab10f200401e78bb7a0a
HadiHeikal <hadiiheikal@gmail.com>
2026-09-12 07:59:19 +0300

    Refactor ProfileRepo implementation and update GetProfileUseCase for consistency

--- lib/features/profile/data/repo_impl/profile_repo_impl.dart
+++ lib/features/profile/data/repo_impl/profile_repo_impl.dart
@@ -1,13 +1,15 @@
-import 'package:flower_app/core/network/base_response.dart';
+import 'package:flower_app/config/base_response/base_response.dart';
 import 'package:flower_app/features/profile/data/data_source/remote/profile_remote_data_source.dart';
 import 'package:flower_app/features/profile/domain/entities/profile_entity.dart';
 import 'package:flower_app/features/profile/domain/repos/profile_repo.dart';
 import 'package:injectable/injectable.dart';
 
 @Injectable(as: ProfileRepo)
-class ProfileRemoteDataSourceImpl implements ProfileRepo {
+class ProfileRepoImpl implements ProfileRepo {
   final ProfileRemoteDataSource _profileRemoteDataSource;
-  ProfileRemoteDataSourceImpl(this._profileRemoteDataSource);
+
+  ProfileRepoImpl(this._profileRemoteDataSource);
+
   @override
   Future<BaseResponse<ProfileEntity>> getProfile() async {
     try {
--- lib/features/profile/domain/use_cases/profile_use_case.dart
+++ lib/features/profile/domain/use_cases/profile_use_case.dart
@@ -1,10 +1,14 @@
-import 'package:flower_app/core/network/base_response.dart';
+import 'package:flower_app/config/base_response/base_response.dart';
 import 'package:flower_app/features/profile/domain/entities/profile_entity.dart';
 import 'package:flower_app/features/profile/domain/repos/profile_repo.dart';
+import 'package:injectable/injectable.dart';
 
+@injectable
 class GetProfileUseCase {
   final ProfileRepo _profileRepo;
+
   GetProfileUseCase(this._profileRepo);
+
   Future<BaseResponse<ProfileEntity>> call() {
     return _profileRepo.getProfile();
   }
