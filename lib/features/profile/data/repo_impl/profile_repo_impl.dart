03ecdf07e987d488d7e2ab573db09f5f98f1e031
HadiHeikal <hadiiheikal@gmail.com>
2026-09-14 09:26:13 +0300

    Add updateProfile method to ProfileRepo and implement ProfileRepoImpl; create UpdateProfileEntity model

--- lib/features/profile/data/repo_impl/profile_repo_impl.dart
+++ lib/features/profile/data/repo_impl/profile_repo_impl.dart
@@ -1,6 +1,8 @@
 import 'package:flower_app/config/base_response/base_response.dart';
 import 'package:flower_app/features/profile/data/data_source/remote/profile_remote_data_source.dart';
+import 'package:flower_app/features/profile/data/models/update_profile_dto.dart';
 import 'package:flower_app/features/profile/domain/entities/profile_entity.dart';
+import 'package:flower_app/features/profile/domain/entities/update_profile_entity.dart';
 import 'package:flower_app/features/profile/domain/repos/profile_repo.dart';
 import 'package:injectable/injectable.dart';
 
@@ -15,6 +17,21 @@ class ProfileRepoImpl implements ProfileRepo {
     try {
       final profile = await _profileRemoteDataSource.getProfile();
       return SuccessResponse(profile.toEntity());
+    } catch (e) {
+      return ErrorResponse(error: e);
+    }
+  }
+
+  @override
+  Future<BaseResponse<ProfileEntity>> updateProfile(
+    UpdateProfileEntity updateProfileEntity,
+  ) async {
+    try {
+      final updateProfileDto = UpdateProfileDto.fromEntity(updateProfileEntity);
+      final updatedProfile = await _profileRemoteDataSource.updateProfile(
+        updateProfileDto,
+      );
+      return SuccessResponse(updatedProfile.toEntity());
     } catch (e) {
       return ErrorResponse(error: e);
     }
--- lib/features/profile/domain/repos/profile_repo.dart
+++ lib/features/profile/domain/repos/profile_repo.dart
@@ -1,6 +1,11 @@
 import 'package:flower_app/config/base_response/base_response.dart';
 import 'package:flower_app/features/profile/domain/entities/profile_entity.dart';
+import 'package:flower_app/features/profile/domain/entities/update_profile_entity.dart';
 
 abstract interface class ProfileRepo {
   Future<BaseResponse<ProfileEntity>> getProfile();
+
+  Future<BaseResponse<ProfileEntity>> updateProfile(
+    UpdateProfileEntity updateProfileEntity,
+  );
 }
